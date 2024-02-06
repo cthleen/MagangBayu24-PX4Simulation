5cd6e868f0ebe7b407ef5ac627cd53238fe4019e
cthleen <154051577+cthleen@users.noreply.github.com>
2024-02-06 19:23:52 +0700

    Add files via upload

--- cpp_pubsub/src/tugas2/rectangle.cpp
+++ cpp_pubsub/src/tugas2/rectangle.cpp
@@ -55,9 +55,7 @@ private:
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
 
-	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
-
-	uint64_t offboard_setpoint_counter_;   //!< counter for the number of setpoints sent
+	uint64_t offboard_setpoint_counter_;
 	uint8_t waypoint_index;
 	rclcpp::Time last_publish_time_; 
 
@@ -69,22 +67,20 @@ private:
 void OffboardControl::arm()
 {
 	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
-
 	RCLCPP_INFO(this->get_logger(), "Arm command send");
 }
 
 void OffboardControl::disarm()
 {
 	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
-
 	RCLCPP_INFO(this->get_logger(), "Disarm command send");
 }
 
- void OffboardControl::land()
-  {
+void OffboardControl::land()
+{
       publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND, 0.0);
       RCLCPP_INFO(this->get_logger(), "Land command send");
-  }
+}
 
 void OffboardControl::publish_offboard_control_mode()
 {
@@ -94,7 +90,6 @@ void OffboardControl::publish_offboard_control_mode()
 	msg.acceleration = false;
 	msg.attitude = false;
 	msg.body_rate = false;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	offboard_control_mode_publisher_->publish(msg);
 }
 
@@ -114,7 +109,6 @@ void OffboardControl::publish_trajectory_setpoint()
 	msg.position = {waypoints[waypoint_index][0], waypoints[waypoint_index][1], waypoints[waypoint_index][2]};
 	msg.yaw = waypoints[waypoint_index][3];
 	RCLCPP_INFO(this->get_logger(), "Current Target Position: x=%.2f, y=%.2f, z=%.2f, yaw=%.2f", msg.position[0], msg.position[1], msg.position[2], msg.yaw);
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	if ((this->get_clock()->now() - last_publish_time_).seconds() >= 10.0) {
         this->waypoint_index++;
 		if(this->waypoint_index>6){
@@ -142,7 +136,6 @@ void OffboardControl::publish_vehicle_command(uint16_t command, float param1, fl
 	msg.source_system = 1;
 	msg.source_component = 1;
 	msg.from_external = true;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	vehicle_command_publisher_->publish(msg);
 }
 
--- cpp_pubsub/src/tugas3/star.cpp
+++ cpp_pubsub/src/tugas3/star.cpp
@@ -62,8 +62,6 @@ private:
 	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
 	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
 
-	std::atomic<uint64_t> timestamp_;  
-
 	uint64_t offboard_setpoint_counter_;   
 	uint8_t waypoint_index;
     uint8_t is_land;
@@ -77,22 +75,20 @@ private:
 void OffboardControl::arm()
 {
 	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
-
 	RCLCPP_INFO(this->get_logger(), "Arm command send");
 }
 
 void OffboardControl::disarm()
 {
 	publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
-
 	RCLCPP_INFO(this->get_logger(), "Disarm command send");
 }
 
- void OffboardControl::land()
-  {
-      publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND, 0.0);
-      RCLCPP_INFO(this->get_logger(), "Land command send");
-  }
+void OffboardControl::land()
+{
+    publish_vehicle_command(VehicleCommand::VEHICLE_CMD_NAV_LAND, 0.0);
+    RCLCPP_INFO(this->get_logger(), "Land command send");
+}
 
 void OffboardControl::publish_offboard_control_mode()
 {
@@ -102,7 +98,6 @@ void OffboardControl::publish_offboard_control_mode()
 	msg.acceleration = false;
 	msg.attitude = false;
 	msg.body_rate = false;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	offboard_control_mode_publisher_->publish(msg);
 }
 
@@ -122,7 +117,6 @@ void OffboardControl::publish_trajectory_setpoint()
 	msg.position = {waypoints[waypoint_index][0], waypoints[waypoint_index][1], waypoints[waypoint_index][2]};
 	msg.yaw = waypoints[waypoint_index][3];
 	RCLCPP_INFO(this->get_logger(), "Current Target Position: x=%.2f, y=%.2f, z=%.2f, yaw=%.2f", msg.position[0], msg.position[1], msg.position[2], msg.yaw);
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	if ((this->get_clock()->now() - last_publish_time_).seconds() >= 10.0) {
         this->waypoint_index++;
 		if(this->waypoint_index==7){
@@ -144,7 +138,6 @@ void OffboardControl::publish_vehicle_command(uint16_t command, float param1, fl
 	msg.source_system = 1;
 	msg.source_component = 1;
 	msg.from_external = true;
-	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	vehicle_command_publisher_->publish(msg);
 }
 
